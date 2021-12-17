03bce4d9c63224631de2d7f1e673b1f9c8e88fad
Maria Bychkova <mar.byckova@gmail.com>
2021-12-17 23:16:58 +0300

    Bonus miles calculator

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
 public class Main {
-
+    public static void main(String[] args) {
+        int ticketPrice = 1070;
+        int mileCost = 20;
+        int bonusMiles = ticketPrice / mileCost;
+        System.out.println(bonusMiles);
+    }
 }
